af0b62deb857e420f4b5cc82256f1a9e891b3b4a
duongtiendat283251.github.io <89386979+Duongtiendat93@users.noreply.github.com>
2022-05-02 15:45:58 +0700

    re-render list input

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import { useState } from 'react';
 function App() {
 const [name,setName] = useState("datdt")
 const [address,setAddress] = useState("")
+const [todos,setTodos] = useState([])
   const handlerSubmit = (e) => {
-      setName(address)
-      setAddress("")
+    if(!address){
+      alert("sytem")
+      return;
+    }
+    setTodos([...todos,address])
+    setAddress("")
   }
   const handlerClickinput = (e) =>{
       setAddress(e.target.value)
   }
 
   return (
+  
     <div className="App">
       <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello would with Reqact and {name} !</h1>
+        <ul>
+          {todos.map((todo,index) =>{
+            return(
+              <li key={index}>{todo}</li>
+            )
+          })}
+        </ul>
         <input type="text" value={address} onChange={e => handlerClickinput(e)} className="text-black"/>
         <button type="button" onClick={e => handlerSubmit(e)}>ClickMe</button>
       </header>
