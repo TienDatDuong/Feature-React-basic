4322873be8d70023922092d1b15b44b5cfd0f95b
duongtiendat283251.github.io <89386979+Duongtiendat93@users.noreply.github.com>
2022-05-02 14:47:33 +0700

    logic onchange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react';
 
 function App() {
+const [name,setName] = useState("datdt")
+const [address,setAddress] = useState("")
+  const handlerSubmit = (e) => {
+      setName(address)
+      setAddress("")
+  }
+  const handlerClickinput = (e) =>{
+      setAddress(e.target.value)
+  }
+
   return (
     <div className="App">
       <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello would with Reqact and {name} !</h1>
+        <input type="text" value={address} onChange={e => handlerClickinput(e)} className="text-black"/>
+        <button type="button" onClick={e => handlerSubmit(e)}>ClickMe</button>
       </header>
     </div>
   );
